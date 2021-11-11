8b1f01f65e32e3ba6647a3fce432ccdb954e96cc
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-11 10:22:50 -0700

    Author's PAge Complete

--- flask_app/controllers/authors.py
+++ flask_app/controllers/authors.py
@@ -4,27 +4,21 @@ from flask import render_template, redirect, request, session, flash
 from flask_app import app
 from flask_app.models.model_author import Author
 
-@app.route('/')
-def index():
-    return redirect('/authors')
 
-@app.route('/authors')
+@app.route('/')
 def authors():
     authors = Author.get_all()
     return render_template("authors.html", authors=authors)
 
 @app.route('/author/create', methods=['POST'])
 def create_author():
-    print(request.form)
-    id = Author.create(request.form)
-    return redirect('/authors')
+    Author.create(request.form)
+    return redirect('/')
+
+@app.route('/author_info/<int:id>')
+def authorInfo(id):
+    id_data = { 'id' : id }
+    author_data = Author.get_one_with_favorites_m2m(id_data)
+    return render_template('author_info.html', author_data)
 
 
-
-# # Left Join
-# @app.route('/dojo/<int:id>')
-# def dojo(id):
-#     data = {
-#     "id": id
-#     }
-#     return render_template('dojo_info.html', dojo=Author.get_one_with_ninjas(data))   
--- flask_app/models/model_author.py
+++ flask_app/models/model_author.py
@@ -9,6 +9,7 @@ class Author:
         self.last_name = data['last_name']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.books = []
 
     # ***CREATE***
     @classmethod
@@ -23,29 +24,27 @@ class Author:
     def get_all(cls):
         query = "SELECT * FROM authors;"
         results = connectToMySQL('books').query_db(query)
-        authors = []
-        for author in results:
-            authors.append(cls(author))
-        return authors 
+        return results 
 
 	# # (joining two groups)  
     # # add... from .model_(user/no()) import (User/no()) **to top of page 
 	# # add... self.(users/no()) = [] **to class(User/no()):
-    # @classmethod
-    # def get_one_with_ninjas(cls, data ):
-    #     print(data)
-    #     query = "SELECT * FROM (userS/no()) LEFT JOIN ninjas on (userS/no()).id = ninjas.dojo_id WHERE dojos.id = %(id)s;"
-    #     results = connectToMySQL('***userS***').query_db(query,data)
-    #     dojo = cls(results[0])
-    #     for row in results:
-    #         n = {
-    #             'id': row['ninjas.id'],
-    #             'first_name': row['first_name'],
-    #             'last_name': row['last_name'],
-    #             'age': row['age'],
-    #             'created_at': row['ninjas.created_at'],
-    #             'updated_at': row['ninjas.updated_at']
-    #         }
-    #         # users = [] in self dictionary
-    #         dojo.authors.append( Author(n) )
-    #     return dojo
+    @classmethod
+    def get_one_with_favorites_m2m(cls, data ):
+        print(data)
+        query = "SELECT * FROM authors LEFT JOIN favorites ON authors.id = favorites.author_id LEFT JOIN books ON favorites.book_id = books.id WHERE authors.id = %(data)s;"
+        results = connectToMySQL('books').query_db(query,data)
+        author = cls(results[0])
+        print(author)
+        for row in results:
+            a = {
+                'id': row['author.id'],
+                'first_name': row['first_name'],
+                'last_name': row['last_name'],
+                'age': row['age'],
+                'created_at': row['ninjas.created_at'],
+                'updated_at': row['ninjas.updated_at']
+            }
+            # users = [] in self dictionary
+            dojo.authors.append( Author(a) )
+        return dojo
