eacbccc514341834eb5e8dca59740af64b12ae6c
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-11 10:47:43 -0700

    Book Page Complete

--- flask_app/models/model_book.py
+++ flask_app/models/model_book.py
@@ -1,5 +1,30 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 
+
+
 class Book:
     def __init__(self,data):
         self.id = data['id']
+        self.title = data['title']
+        self.num_of_pages = data['num_of_pages']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+
+    # ***CREATE***
+    @classmethod
+    def create(cls,data):
+        query = "INSERT INTO books (title, num_of_pages) VALUES (%(title)s, %(num_of_pages)s);"
+        book_id = connectToMySQL('books').query_db(query,data)
+        return book_id
+
+
+    #  ***Retreive***
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM books;"
+        results = connectToMySQL('books').query_db(query)
+        return results 
+
